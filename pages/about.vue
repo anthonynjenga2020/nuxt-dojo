877f63793b7fdb2ab320d18ec61adfeb4bf109a0
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-04-15 21:49:46 +0300

    new pages folder with index.vue

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+       <h2>About</h2> 
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Aut nam fuga aspernatur sint necessitatibus ducimus eveniet quas harum vitae dolore!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Aut nam fuga aspernatur sint necessitatibus ducimus eveniet quas harum vitae dolore!</p>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
