42fab7b0048593d4fdfb0e3cb00995b9c7009c82
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-04-18 21:04:45 +0300

    learnt about nuxt-layouts and routing

---
+++ pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Sequi ullam repellendus qui exercitationem maxime eius ab minima veritatis fuga nisi.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout: 'products'
+    })
+</script>
+
+<style scoped>
+
+</style>
