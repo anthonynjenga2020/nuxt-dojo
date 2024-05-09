c7ea7adfb7cf2b7de42862fdcf906ee51586b6db
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-05-09 12:28:54 +0300

    dynamic server routes

--- pages/about.vue
+++ pages/about.vue
@@ -10,11 +10,7 @@
 </template>
 
 <script setup>
-const { data } = await useFetch('/api/ninja?name=mario', {
-    method: 'post',
-    body: { age: 30 }
-})
-
+const { data } = await useFetch('/api/currency/GBP')
 </script>
 
 <style scoped>
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,11 +1,11 @@
 export default defineEventHandler(async (event) => {
-    //handle query params
-    const query = getQuery(event)
+    // //handle query params
+    // const query = getQuery(event)
 
-    //handle post data
-    const { age } = await readBody(event)
+    // //handle post data
+    // const { age } = await readBody(event)
 
-    return{
-        message: `Hello, ${query.name}! Your are ${age} years old.`
-    }
+    //api call with private key
+    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_7gr08wdpI5zbZPf88G3o3iweVM5WYd2CdKGj8y8y')
+    return data
 }) 
